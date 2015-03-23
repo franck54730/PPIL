de7ed3e2243d361cf45e30fbc51ce850bcdc6260
Franck54730 <franck_audiger@hotmail.com>
2015-03-23 19:40:52 +0100

    pour que tout le monde est database.php modifier le a votre guise je le remet dans le .gitignore juste apres

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfzzZZZZERWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'etnjvoZJE234zr963TZFTJ46ERHISYGnjhterburht');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657487898498753542496749683645');
+	Configure::write('Security.cipherSeed', '2586528652625865');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
