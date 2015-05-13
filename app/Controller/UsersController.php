8aa1628c4073d0d3852c586d49027b62290a5488
Florine <florine.simon54@gmail.com>
2015-05-13 14:39:07 +0200

    Ajout de la vérification de la date lors de l'inscription et de la mise à jour du profi

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -52,31 +52,37 @@ class UsersController extends AppController {
 		if ($this->request->is('post')) {
 			$d =$this->request->data;
 			$d['User']['id'] = null;
+			$jour = $d['User']['date_de_naissance']['day'];
+			$mois = $d['User']['date_de_naissance']['month'];
+			$annee = $d['User']['date_de_naissance']['year'];
 			// Si le mot de passe n'est pas vide et que les 2 mots de passes concordent
 			if (!empty($d['User']['mot_de_passe']) && $d['User']['mot_de_passe'] == $d['User']['mot_de_passe_verif']) {
 				$d['User']['mot_de_passe'] = Security::hash(trim($d['User']['mot_de_passe']), 'md5', $this->salt);
 				// Si le mail n'est pas vide
 				if (!empty($d['User']['mail'])) {
-					// Si on arrive � l'�crire en BDD
-					if($this->User->save($d,true,array('nom','prenom','date_de_naissance','sexe','mail','mot_de_passe','photo'))){
-						// On r�cup�re l'objet qu'on vient d'�crire en BDD (pour r�cup�rer son ID)
-						$user = $this->User->find('first', array('conditions' => array('User.mail' => $d['User']['mail'])));
-						$user=$user['User'];
-						// On l'�crit en Session, l'utilisateur est maintenant inscrit ET connect�
-						$this->Session->write("User",$user);
-						$this->Session->setFlash("Votre compte a bien &eacute;t&eacute; cr&eacute;e","notif");
-		        		$this->redirect('/');
+					if(checkdate($mois,$jour,$annee)){
+						// Si on arrive � l'�crire en BDD
+						if($this->User->save($d,true,array('nom','prenom','date_de_naissance','sexe','mail','mot_de_passe','photo'))){
+							// On r�cup�re l'objet qu'on vient d'�crire en BDD (pour r�cup�rer son ID)
+							$user = $this->User->find('first', array('conditions' => array('User.mail' => $d['User']['mail'])));
+							$user=$user['User'];
+							// On l'�crit en Session, l'utilisateur est maintenant inscrit ET connect�
+							$this->Session->write("User",$user);
+							$this->Session->setFlash("Votre compte a bien &eacute;t&eacute; cr&eacute;e","notif");
+			        		$this->redirect('/');
+			        	}else{
+			        		$this->Session->setFlash("Email existant");
+			        	}
+		        	}else{
+		        		$this->Session->setFlash("La date n'est pas correcte.");
 		        	}
-		        	else{
-		        		$this->Session->setFlash("Email existant");
-		        	}
-				}else{
-					$this->Session->setFlash("Veuillez corriger vos erreurs","notif",array('type'=>'error'));
-				}
+		        }    
+			}else{
+				$this->Session->setFlash("Veuillez corriger vos erreurs","notif",array('type'=>'error'));
 			}
-			else{
-				$this->Session->setFlash("Les 2 mots de passe ne correspondent pas");
-			}				
+			
+		}else{
+			$this->Session->setFlash("Les 2 mots de passe ne correspondent pas");
 		}
 	}
 
@@ -98,16 +104,25 @@ class UsersController extends AppController {
 		if(!empty($this->data)){
 			$user['nom'] = $this->data['User']['nom'];
 			$user['prenom'] = $this->data['User']['prenom'];
-			$datenaiss = $this->data['User']['date_de_naissance']['year'].'-'.$this->data['User']['date_de_naissance']['month'].'-'.$this->data['User']['date_de_naissance']['day'];
+
+			$jour = $this->data['User']['date_de_naissance']['day'];
+			$mois = $this->data['User']['date_de_naissance']['month'];
+			$annee = $this->data['User']['date_de_naissance']['year'];
+			$datenaiss = $annee.'-'.$mois.'-'.$jour;
 			$user['date_de_naissance'] = $datenaiss;
 			$user['sexe'] = $this->data['User']['sexe'];
-			$this->Session->write("User",$user);
-			if($this->User->save($user)){
-				$this->Session->setFlash("Vos modifications ont bien &eacute;t&eacute; enregistr&eacute;es","notif");
-			    $this->redirect('/users/profil');
+			if(checkdate($mois, $jour, $annee)){
+				$this->Session->write("User",$user);
+				if($this->User->save($user)){
+					$this->Session->setFlash("Vos modifications ont bien &eacute;t&eacute; enregistr&eacute;es","notif");
+				    $this->redirect('/users/profil');
+				}
+				else{
+					$this->Session->setFlash("Erreur lors de la modification de vos donn&eacute;es.");
+				}
 			}
 			else{
-				$this->Session->setFlash("Erreur");
+				$this->Session->setFlash("La date n'est pas correcte.");
 			}
 		}
 	}
